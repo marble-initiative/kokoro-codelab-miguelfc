969c3ca4e001578ce7d80143d8dad6a82129159a
Miguel Fernandes <miguelfc@google.com>
2022-10-04 15:15:52 +0200

    Moved to python sample

---
+++ sample_test.py
@@ -0,0 +1,15 @@
+"""
+Sample Test
+"""
+
+import sample
+
+
+def test_sum_two_positive():
+    """Test the sum_two function"""
+    assert sample.sum_two(1) == 3
+
+
+def test_sum_two_negative():
+    """Negative test of sum_two function"""
+    assert sample.sum_two(-2) == 0
